653dd9018eedd241e668e58354e1ff8e38d2d449
Cursed Entertainment <cursedsoft@gmail.com>
2024-02-10 15:00:14 +0200

    ...

--- scripts/cover-image.js
+++ scripts/cover-image.js
@@ -1,4 +1,31 @@
-var coverPath = '../cover.png';
+var bookCover = document.getElementById('bookCover');
+var isOpen = false;
 
-// Set the background image for the cover photo
-document.querySelector('.cover-photo').style.backgroundImage = 'url(' + coverPath + ')';
+
+var book = document.querySelector('.book');
+
+
+book.addEventListener('mouseenter', function () {
+    isOpen = true;
+    updateBookCover();
+});
+
+book.addEventListener('mouseleave', function () {
+    isOpen = false;
+    updateBookCover();
+});
+
+function updateBookCover() {
+    var closedCover = bookCover.querySelector('[alt="Closed Book Cover"]');
+    var openCover = bookCover.querySelector('[alt="Open Book Cover"]');
+
+    if (isOpen) {
+
+        closedCover.style.display = 'none';
+        openCover.style.display = 'block';
+    } else {
+
+        closedCover.style.display = 'block';
+        openCover.style.display = 'none';
+    }
+}
